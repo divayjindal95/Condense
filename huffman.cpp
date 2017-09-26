d5de52e2518753a659a928005358bdb3e3ec6d95
= <=>
2017-09-26 17:54:50 +0530

    encoding for huffman completed, need to see for decoding

--- huffman.cpp
+++ huffman.cpp
@@ -13,15 +13,35 @@ struct node{
 	int val;
 };
 
+// HEAP
+
+
+/* Generic Print function
+template <typename T>
+void print(T arr){
+	T::iterator it;
+	for(it=arr.begin();it!=arr.end();it++)
+		return;
+}
+*/
+
+void inorder(node *root){
+	//cout<<root<<"\n";
+	if(root==NULL)	return;
+	inorder(root->left);
+	cout<<root->freq;
+	inorder(root->right);
+}
+
 bool cmp(struct node *n1, struct node *n2){
-	return n1->freq < n2->freq;
+	return n1->freq > n2->freq;
 }
 
 void make_table(node *root, string code){
-	cout<<"hello";
+
 	if(root->left==NULL && root->right==NULL){
 		lookup_table[root->val]=code;
-		cout<<root->val<<" "<<code;
+		cout<<root->val<<" "<<code<<endl;
 		return;
 	}
 
@@ -29,12 +49,12 @@ void make_table(node *root, string code){
 	if (root->right!=NULL) make_table(root->right,code+'1');
 
 }
-s
+
 void encode(string s){
 
 	int freq[char_num];	
 	memset(freq,0,sizeof(freq));
-	for(int i=0;i<char_num;i++)
+	for(int i=0;i<s.length();i++)
 		freq[s[i]]++;
 
 	std::vector< struct node* > vec;
@@ -49,31 +69,52 @@ void encode(string s){
 		}
 	}
 
-
 	make_heap(vec.begin(),vec.end(), cmp);
 	while(vec.size()!=1){
-		pop_heap(vec.begin(),vec.end());
-		node *n1=vec[vec.size()-1];
+		for(int i=0;i<vec.size();i++){
+			cout<<vec[i]->val<<" "<<vec[i]->freq<<" "<<vec[i]->left<<" "<<vec[i]->right<<" "<<vec[i]<<endl;
+		}
+		cout<<"\n";
+		pop_heap(vec.begin(),vec.end(),cmp);
+
+		node *n1 =vec[vec.size()-1];
+
 		vec.pop_back();
 
-		pop_heap(vec.begin(),vec.end());
+		pop_heap(vec.begin(),vec.end(),cmp);
 		node *n2=vec[vec.size()-1];
 		vec.pop_back();
 
-		struct node *n;
+		struct node *n=new struct node;
 		n->freq=n1->freq+n2->freq;
 		n->left=n1;
 		n->right=n2;
 		n->val=-1;
 		vec.push_back(n);
-		push_heap(vec.begin(),vec.end());
+		push_heap(vec.begin(),vec.end(),cmp);
 	}
+
+	for(int i=0;i<vec.size();i++){
+		cout<<vec[i]->val<<" "<<vec[i]->freq<<" "<<vec[i]->left<<" "<<vec[i]->right<<" "<<vec[i]<<endl;
+	}
+	cout<<"\n";
+
 	node *root = vec[0];
-	
+	//cout<<root->left<<" "<<root->left->freq<<endl;
+	//cout<<root->right<<" "<<root->right->freq<<endl;
+	//cout<<root->left->left->left;
+	//inorder(root);
 	make_table(root,"");
 
 	for(std::map<char, string>::iterator it=lookup_table.begin();it!=lookup_table.end();it++)
 		cout<<it->first<<" " <<it->second<<"\n";
+
+	vector<string> buff;
+	for(int i=0;i<s.length();i++)
+		buff.push_back(lookup_table[s[i]]);
+
+	for(int i=0;i<buff.size();i++)
+		cout<<buff[i];
 
 }
 
@@ -83,9 +124,7 @@ int main(){
 	string s;
 	getline(cin,s);
 
-
 	encode(s);
-
 
 	return 0;
 }
