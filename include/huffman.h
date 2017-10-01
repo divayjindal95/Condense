0c4de880484ea3fb720f3c2b2ab2620bcb2b2872
= <=>
2017-10-01 18:49:48 +0530

    added cmake , create heirarchy

---
+++ include/huffman.h
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int char_num=256;
+
+std::map<char, string> lookup_table;
+
+struct node{
+	int freq;
+	struct node *left;
+	struct node *right;
+	int val;
+};
+
+// HEAP
+
+
+/* Generic Print function
+template <typename T>
+void print(T arr){
+	T::iterator it;
+	for(it=arr.begin();it!=arr.end();it++)
+		return;
+}
+*/
+
+void inorder(node *root){
+	//cout<<root<<"\n";
+	if(root==NULL)	return;
+	inorder(root->left);
+	cout<<root->freq;
+	inorder(root->right);
+}
+
+bool cmp(struct node *n1, struct node *n2){
+	return n1->freq > n2->freq;
+}
+
+void make_table(node *root, string code){
+
+	if(root->left==NULL && root->right==NULL){
+		lookup_table[root->val]=code;
+		cout<<root->val<<" "<<code<<endl;
+		return;
+	}
+
+	if (root->left!=NULL) make_table(root->left,code+'0');
+	if (root->right!=NULL) make_table(root->right,code+'1');
+
+}
+
+void huffman_encode(string s){
+
+	int freq[char_num];	
+	memset(freq,0,sizeof(freq));
+	for(int i=0;i<s.length();i++)
+		freq[s[i]]++;
+
+	std::vector< struct node* > vec;
+	for(int i=0;i<char_num;i++){
+		if(freq[i]!=0){
+			struct node *n = new struct node;
+			n->left=NULL;
+			n->right=NULL;
+			n->freq=freq[i];
+			n->val=i;
+			vec.push_back(n);
+		}
+	}
+
+	make_heap(vec.begin(),vec.end(), cmp);
+	while(vec.size()!=1){
+		for(int i=0;i<vec.size();i++){
+			cout<<vec[i]->val<<" "<<vec[i]->freq<<" "<<vec[i]->left<<" "<<vec[i]->right<<" "<<vec[i]<<endl;
+		}
+		cout<<"\n";
+		pop_heap(vec.begin(),vec.end(),cmp);
+
+		node *n1 =vec[vec.size()-1];
+
+		vec.pop_back();
+
+		pop_heap(vec.begin(),vec.end(),cmp);
+		node *n2=vec[vec.size()-1];
+		vec.pop_back();
+
+		struct node *n=new struct node;
+		n->freq=n1->freq+n2->freq;
+		n->left=n1;
+		n->right=n2;
+		n->val=-1;
+		vec.push_back(n);
+		push_heap(vec.begin(),vec.end(),cmp);
+	}
+
+	for(int i=0;i<vec.size();i++){
+		cout<<vec[i]->val<<" "<<vec[i]->freq<<" "<<vec[i]->left<<" "<<vec[i]->right<<" "<<vec[i]<<endl;
+	}
+	cout<<"\n";
+
+	node *root = vec[0];
+	//cout<<root->left<<" "<<root->left->freq<<endl;
+	//cout<<root->right<<" "<<root->right->freq<<endl;
+	//cout<<root->left->left->left;
+	//inorder(root);
+	make_table(root,"");
+
+	for(std::map<char, string>::iterator it=lookup_table.begin();it!=lookup_table.end();it++)
+		cout<<it->first<<" " <<it->second<<"\n";
+
+	vector<string> buff;
+	for(int i=0;i<s.length();i++)
+		buff.push_back(lookup_table[s[i]]);
+
+	for(int i=0;i<buff.size();i++)
+		cout<<buff[i];
+
+}
+
+/*
+int main(){
+
+	printf("input text\n");
+	string s;
+	getline(cin,s);
+
+	encode(s);
+
+	return 0;
+}
+*/
---
+++ include/lz77.h
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int LENGTH=100;
+int DISTANCE=100;
+
+std::vector<string> lz77_encode(string s){
+	std::vector<string> buff;
+	for(int i=0;i<s.length();){
+		int length=1,distance=0;
+		for(int j=max(0,i-DISTANCE);j<i;j++){
+			int k=0;
+			while(k<LENGTH && s[j+k]==s[i+k] && j<i && i<s.length())
+				k++;
+
+			if(k>length){
+				length=k;
+				distance=i-j;
+			}
+		}
+		if(length>1){
+			string temp;
+			temp="<"+distance;
+			temp+=length;
+			temp+=",";
+			temp+=">";
+			buff.push_back(temp);
+			cout<<"<"<<distance<<","<<length<<">";
+		}
+		else{
+			buff.push_back(s[i]+"");
+			cout<<s[i];
+		}
+		i=i+length;
+	}
+	return buff;
+}
+
+void decode(string s){
+	return;
+}
+
+/*
+int main(){
+
+	printf("input text\n");
+	string s;
+	getline(cin,s);
+
+	encode(s);
+
+	return 0;
+}
+*/
